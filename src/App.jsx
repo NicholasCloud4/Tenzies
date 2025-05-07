3243ccfdad9bdcc3a278305bf64f48d2ffe70a6b
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-05-07 13:46:07 -0400

    Created Die component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Die from './components/Die'
 
-function App() {
-  const [count, setCount] = useState(0)
+/**
+ * Challenge:
+ * 
+ * - Create a Die component that takes a `value` prop. Should
+ *   render a button with that value displayed.
+ * - Render 10 instances of the Die component (manually)
+ *      - Provide a number between 1-6 for the value on each
+ *        for now
+ * - Style the <main> and <Die> components 
+ *   to look like they do in the slide
+ *      - Hints: Create a container to hold the 10 instances
+ *        of the Die component, and use CSS Grid to lay them
+ *        out evenly in 2 rows of 5 columns
+ *      - Use flexbox on main to center the dice container
+ *        in the center of the page
+ */
 
+
+export default function App() {
   return (
-    <>
-      <main>Type something here</main>
-    </>
+    <main>
+      <div className='dice-container'>
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={6} />
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+      </div>
+
+    </main>
   )
 }
 
-export default App
