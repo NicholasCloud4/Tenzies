0d2e9e947c6055c833c9b9db54d200f5c5d81358
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-05-08 10:46:18 -0400

    Added Confetti

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import React, { useState } from 'react'
 import Die from './components/Die'
 import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
 
 
 
 export default function App() {
 
     const [dice, setDice] = useState(generateAllNewDice())
+
+    /**
+     * Challenge:
+     * Log "Game won!" to the console only if the 2 winning
+     * conditions are met.
+     * 
+     * 1. all the dice are being held, and
+     * 2. all the dice have the same value
+     * 
+     * For now, no need to even save a variable!
+     */
+
+    /**
+     * Challenge part 2:
+     * 1. Create a new `gameWon` variable.
+     * 2. If `gameWon` is true, change the button text to
+     *    "New Game" instead of "Roll"
+     */
+
+    let gameWon = dice.every(die => die.isHeld === true) && dice.every(die => die.value === dice[0].value)
+
+
 
     function generateAllNewDice() {
         let arr = []
@@ -26,15 +49,6 @@ export default function App() {
         console.log(id)
     }
 
-    /**
-     * Challenge: Update the `rollDice` function to not just roll
-     * all new dice, but instead to look through the existing dice
-     * to NOT role any that are being `held`.
-     * 
-     * Hint: this will look relatively similiar to the `hold`
-     * function below. When we're "rolling" a die, we're really
-     * just updating the `value` property of the die object.
-     */
 
     function rollDice() {
         //setDice(generateAllNewDice())
@@ -50,6 +64,7 @@ export default function App() {
 
     return (
         <main>
+            {gameWon === true && <Confetti />}
 
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
@@ -59,7 +74,8 @@ export default function App() {
                 {diceElements}
             </div>
 
-            <button className='roll-dice' onClick={rollDice}>Roll Dice</button>
+            {gameWon === true ? <button className='roll-dice' onClick={rollDice}>New Game</button> :
+                <button className='roll-dice' onClick={rollDice}>Roll Dice</button>}
 
         </main>
     )
